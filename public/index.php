3e3b1734a91df94826fcbc95f16e137a46c5e711
IsRylo <91057829+IsRylo@users.noreply.github.com>
2022-05-06 11:25:24 +0800

    Update index.php

--- public/index.php
+++ public/index.php
@@ -3,3 +3,18 @@ if( !session_id() ) session_start();
 require_once '../app/init.php';
 
 $app = new app;
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>hello</p>
+</body>
+</html>
